697edf6025d2ba307fd258243870151d2634b4a1
Tang <forbiddensite403@hotmail.com>
2019-03-08 12:17:13 +0800

    Fix issues with database.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -8,25 +8,25 @@ const database = new MySQLDB(config.mysql_config);
 database.connect();
 
 router.get('/test', async (req, res) => {
-    res.status(200).send({
-        "ping": "pong"
-    });
+  res.status(200).send({
+    ping: 'pong',
+  });
 });
 
-router.post('/register', async(req, res) => {
-    const { name, username, password } = req.body;
-    const user = await database.getStaff(username);
-    if (user.length) {
-        return res.status(409).send({
-            "Error": "Username already exists"
-        });
-    }
-
-    const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-    await database.createStaff(name, username, hashPassword);
-    return res.status(200).send({
-        "Success": "User successfully created"
+router.post('/register', async (req, res) => {
+  const { name, username, password } = req.headers;
+  const user = await database.getStaff(username);
+  if (user.length) {
+    return res.status(409).send({
+      Error: 'Username already exists',
     });
-})
+  }
+
+  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  await database.createStaff(name, username, hashPassword);
+  return res.status(200).send({
+    Success: 'User successfully created',
+  });
+});
 
 export default router;
--- src/database.js
+++ src/database.js
@@ -6,22 +6,19 @@ class MySQLDB {
   }
 
   connect() {
-    console.log("Connecting to MYSQL Database...");
+    console.log('Connecting to MYSQL Database...');
     return new Promise((resolve, reject) => {
       // eslint-disable-next-line consistent-return
       this.connection.connect(err => {
-        if (err)
-          return reject(err);
-        console.log("Connection to MySQL Database established!");
+        if (err) return reject(err);
+        console.log('Connection to MySQL Database established!');
         resolve();
       });
-      this.connection.on('error', (err) => {
-        console.error("DB error:", err)
-        if(err.code === 'PROTOCOL_CONNECTION_LOST')
-          this.connect();
-        else
-          throw err;
-      })
+      this.connection.on('error', err => {
+        console.error('DB error:', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') this.connect();
+        else throw err;
+      });
     });
   }
 
@@ -29,42 +26,53 @@ class MySQLDB {
     return new Promise((resolve, reject) => {
       // eslint-disable-next-line consistent-return
       this.connection.query(sql, args, (err, rows) => {
-        if (err)
-          return reject(err);
+        if (err) return reject(err);
         resolve(rows);
-      } );
-    } );
+      });
+    });
   }
 
   close() {
     return new Promise((resolve, reject) => {
       // eslint-disable-next-line consistent-return
       this.connection.end(err => {
-        if (err)
-          return reject(err);
-        console.log("Connection to MySQL Database closed.");
+        if (err) return reject(err);
+        console.log('Connection to MySQL Database closed.');
         resolve();
-      } );
-    } );
+      });
+    });
   }
 
   getStaff(username) {
-    const res = this.query("SELECT * FROM users WHERE username = ?", username)
+    const res = this.query('SELECT * FROM users WHERE username = ?', [username])
       .then(rows => rows)
       .catch(err => {
-        console.error("Error from getStaff:", err.sqlMessage);
-        return err.code
+        console.error('Error from getStaff:', err.sqlMessage);
+        return err.code;
       });
     return res;
   }
 
   createStaff(name, username, password) {
-    const res = this.query(`INSERT INTO users (name, username, password)
-                            VALUES (?, ?, ?)`, [name, username, password])
+    const res = this.query(
+      `INSERT INTO users (name, username, password)
+                            VALUES (?, ?, ?)`,
+      [name, username, password],
+    )
       .then(rows => rows)
       .catch(err => {
-        console.error("Error from createStaff:", err.sqlMessage);
-        return err.code
+        console.error('Error from createStaff:', err.sqlMessage);
+        return err.code;
+      });
+    return res;
+  }
+
+  getAllIncident() {
+    const res = this.query("SELECT * FROM incidents WHERE status = 'ongoing'")
+      .then(rows => rows)
+      .catch(err => {
+        console.error('Error from getAllIncident:', err.sqlMessage);
+        return err.code;
       });
     return res;
   }
