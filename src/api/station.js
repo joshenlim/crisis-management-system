1ba8fe6e7d4e4724283cf398918d4dc7719919f0
Chee Chuan Bin <chuanbin@hotmail.com>
2019-03-25 21:02:52 +0800

    Update station.js

--- src/api/station.js
+++ src/api/station.js
@@ -7,25 +7,13 @@ const database = new MySQLDB(config.mysql_config);
 database.connect();
 
 router.get('/get_all_station', async (req, res) => {
-  const stations = await database
-    .query('SELECT * FROM fire_station')
-    .then(rows => rows)
-    .catch(err => {
-      console.error('Error from getAllStation:', err.sqlMessage);
-      return res.status(409).send({ Error: err.code });
-    });
+  const stations = await database.getAllStation();
   return res.status(200).send(stations);
 });
 
 router.get('/get_station_by_id', async (req, res) => {
   const { id } = req.query;
-  const stations = await database
-    .query('SELECT * FROM fire_station WHERE id = ?', [id])
-    .then(rows => rows)
-    .catch(err => {
-      console.error('Error from getStationCallsign:', err.sqlMessage);
-      return res.status(409).send({ Error: err.code });
-    });
+  const stations = await database.getStationByID(id);
   return res.status(200).send(stations);
 });
 
