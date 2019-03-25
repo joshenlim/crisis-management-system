4c9b193b6bd80548ece2e11f96135aa676cae60e
Chee Chuan Bin <chuanbin@hotmail.com>
2019-03-25 20:58:32 +0800

    Removed useless API

--- src/database.js
+++ src/database.js
@@ -72,84 +72,6 @@ class MySQLDB {
     return res;
   }
 
-  addAuthority(role_id, staff_id) {
-    var res;
-    switch (role_id) {
-      case 1:
-        {
-          res = this.query(
-            `INSERT INTO ops_operator (staff_id)
-                                VALUES (?)`,
-            [staff_id],
-          )
-            .then(rows => rows)
-            .catch(err => {
-              console.error('Error from addAuthority:', err.sqlMessage);
-              return err.code;
-            });
-        }
-        break;
-      case 2:
-        {
-          res = this.query(
-            `INSERT INTO ops_manager (staff_id)
-                                VALUES (?)`,
-            [staff_id],
-          )
-            .then(rows => rows)
-            .catch(err => {
-              console.error('Error from addAuthority:', err.sqlMessage);
-              return err.code;
-            });
-        }
-        break;
-      case 3:
-        {
-          //To clarify
-          // res = this.query(
-          //   `INSERT INTO ops_gc (staff_id)
-          //                       VALUES (?)`,
-          //   [staff_id],
-          // )
-          //   .then(rows => rows)
-          //   .catch(err => {
-          //     console.error('Error from addAuthority:', err.sqlMessage);
-          //     return err.code;
-          //   });
-        }
-        break;
-      case 4:
-        {
-          res = this.query(
-            `INSERT INTO specialist (staff_id)
-                                VALUES (?)`,
-            [staff_id],
-          )
-            .then(rows => rows)
-            .catch(err => {
-              console.error('Error from addAuthority:', err.sqlMessage);
-              return err.code;
-            });
-        }
-        break;
-      case 5:
-        {
-          res = this.query(
-            `INSERT INTO relations_officer (staff_id)
-                                VALUES (?)`,
-            [staff_id],
-          )
-            .then(rows => rows)
-            .catch(err => {
-              console.error('Error from addAuthority:', err.sqlMessage);
-              return err.code;
-            });
-        }
-        break;
-    }
-    return res;
-  }
-
   getPolicies(roleId) {
     const res = this.query(
       'SELECT policy.id, policy.name FROM role JOIN policy ON role.id = policy.role_id WHERE role.id = ?',
@@ -158,6 +80,26 @@ class MySQLDB {
       .then(rows => rows)
       .catch(err => {
         console.error('Error from createStaff:', err.sqlMessage);
+        return err.code;
+      });
+    return res;
+  }
+
+  getAllStation() {
+    const res = this.query('SELECT * FROM fire_station')
+      .then(rows => rows)
+      .catch(err => {
+        console.error('Error from getAllStation:', err.sqlMessage);
+        return err.code;
+      });
+    return res;
+  }
+
+  getStationByID(id) {
+    const res = this.query('SELECT * FROM fire_station WHERE id = ?', [id])
+      .then(rows => rows)
+      .catch(err => {
+        console.error('Error from getStationCallsign:', err.sqlMessage);
         return err.code;
       });
     return res;
