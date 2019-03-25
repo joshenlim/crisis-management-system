4a171d1e90c55dd9f3bb3bb848a4425438ae4549
Han Xiang <han_xiang27@hotmail.com>
2019-03-25 11:53:40 +0800

    update

--- src/api/manageIncident.js
+++ src/api/manageIncident.js
@@ -143,4 +143,69 @@ router.post('/update', async (req, res) => {
   // res.status(200).send(incidents);
 });
 
+router.get('/get_id', async (req, res) => {
+  const { emergid } = req.query;
+  const incidents = await database
+      .query('SELECT i.* FROM incidents i,civil_emergency e WHERE e.incident_id = ? AND i.incident_id=e.incident_id', [
+      emergid,
+    ])
+      .then(rows => rows)
+      .catch(err => {
+        console.error('Error from getEmergencyIncidentById:', err.sqlMessage);
+        return res.status(409).send({ Error: err.code });
+      });
+  return res.status(200).send(incidents);
+});
+
+router.get('/get', async (req, res) => {
+  const incidents = await database
+    .query("SELECT * FROM incidents WHERE status = 'resolved'")
+    .then(rows => rows)
+    .catch(err => {
+      console.error('Error from getArchivedIncident:', err.sqlMessage);
+      return res.status(409).send({ Error: err.code });
+    });
+  return res.status(200).send(incidents);
+});
+
+router.put('/put', async (req, res) => {
+  const { emergid } = req.query;
+  const incidents = await database
+      .query('UPDATE incidents SET status = resolved WHERE incident_id = ?', [
+      emergid,
+    ])
+      .then(rows => rows)
+      .catch(err => {
+        console.error('Error from putResolvedIncident:', err.sqlMessage);
+        return res.status(409).send({ Error: err.code });
+      });
+  return res.status(201).send(incidents);
+});
+
+router.post('/create', async (req, res) => {
+  const {
+    id,
+    plate_number,
+  } = req.headers;
+
+ await database
+    .query('UPDATE vehicle SET on_off_call = 1 WHERE plate_number = ?', [plate_number],
+      'INSERT INTO vehicle_incident (id, plate_number) VALUES (?, ?)',
+      [
+        id,
+        plate_number,
+      ],
+    )
+    .then(rows => rows)
+    .catch(err => {
+      console.error('Error from dispatchAdditionalUnit:', err.sqlMessage);
+      return res.status(409).send({ Error: err.code });
+    });
+  
+    return res.status(200).send({
+    Success: 'Dispatch on additional unit successfully created',
+  });
+});
+
+
 export default router;
