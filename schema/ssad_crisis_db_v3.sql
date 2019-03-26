555d49b314c11fd4909f575b785c4bc082c847b2
Joshen Lim <joshenlim@Joshens-MacBook-Pro.local>
2019-03-26 19:42:21 +0800

    Rename category table id columns to incident_id for greater clarity

--- schema/ssad_crisis_db_v3.sql
+++ schema/ssad_crisis_db_v3.sql
@@ -8,3 +8,6 @@ ALTER TABLE `incidents` ALTER `status` SET DEFAULT 'DISPATCHED';
 
 ALTER TABLE `vehicle_incident` ADD `veh_status` VARCHAR(255) NOT NULL;
 
+ALTER TABLE `fire_emergency` CHANGE `id` `incident_id` INT(11) NOT NULL;
+ALTER TABLE `med_emergency` CHANGE `id` `incident_id` INT(11) NOT NULL;
+ALTER TABLE `road_traffic_acc` CHANGE `id` `incident_id` INT(11) NOT NULL;
