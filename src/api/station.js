fa64d62ebb53709ecab6dbc1abdfc31ee0402b51
Chee Chuan Bin <chuanbin@hotmail.com>
2019-04-03 15:47:36 +0800

    Added get_dispatched_vehicles api

--- src/api/station.js
+++ src/api/station.js
@@ -23,4 +23,10 @@ router.get('/get_station_vehicles', async (req, res) => {
   return res.status(200).send(vehicles);
 });
 
+router.get('/get_dispatched_vehicles', async (req, res) => {
+  const { incident_id } = req.query;
+  const vehicles = await database.getDispatchedVehicles(incident_id);
+  return res.status(200).send(vehicles);
+});
+
 export default router;
