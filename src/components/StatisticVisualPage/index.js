c58dcf25b4f6c8c29f49b8123af3292ca5b0140b
Tang <forbiddensite403@hotmail.com>
2019-04-07 20:37:40 +0800

    Replicated statistic page layout from figma

--- src/components/StatisticVisualPage/index.js
+++ src/components/StatisticVisualPage/index.js
@@ -12,14 +12,68 @@ class StatisticVisualPage extends Component {
   }
   render() {
     return (
-      <div>
-        <div className={s.container}>
-          <div className={s.card}>
-            <div className={s.title}>Weekly Statistics</div>
+      <div className={s.container}>
+        <div className={s.cardTop}>
+          <div className={s.title}>Daily Statistics</div>
+          <br />
+          <div className={s.body}>
+            <div className={s.leftcol}>
+              <div className={s.hugenum}>4</div>&emsp;
+              <div className={s.text}>
+                Incidents
+                <br /> Opened
+              </div>
+            </div>
+            <div className={s.rightcol}>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Fire Emergency</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Gas Leak</td>
+                  </tr>
+                  <tr>
+                    <td>0</td>
+                    <td>Medical Emergency</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td>Road Traffic Accident</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div className={s.cardTop}>
+          <div className={s.title}>Emergency Incidents</div>
+          <br />
+          <div className={s.body}>
+            <div className={s.leftcol}>
+              <div className={s.hugenum}>2</div>&emsp;
+              <div className={s.text}>
+                Incidents
+                <br /> Escalated
+              </div>
+            </div>
+            <div className={s.rightcol} />
+          </div>
+        </div>
+        <div className={s.cardTop}>
+          <div className={s.title}>Generate Daily Report</div>
+        </div>
+        <div className={s.card}>
+          <div className={s.title}>Weekly Statistics</div>
+          <div className={s.chart}>
             <LineGraph />
           </div>
-          <div className={s.card}>
-            <div className={s.title}>Case Category</div>
+        </div>
+        <div className={s.card}>
+          <div className={s.title}>Case Category</div>
+          <div className={s.chart}>
             <PieChart />
           </div>
         </div>
